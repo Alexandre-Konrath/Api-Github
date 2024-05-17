d178b016bb667fc5874226ea3098251d1d8204a5
Alexandre-Konrath <delimakonrath@gmail.com>
2024-05-17 17:07:08 -0300

    adding the two main routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import MainPage from "./pages/Main";
+import MainPage from "./pages/MainPage";
+import RepositoriesPage from "./pages/RepositoriesPage";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+
+      <Route path="repositories" element={<RepositoriesPage />} />
     </Routes>
   );
 }
