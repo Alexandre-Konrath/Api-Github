d6326652cb9387f4609c9c29886c0ca219c46d9a
Alexandre-Konrath <delimakonrath@gmail.com>
2024-05-17 17:08:21 -0300

    resetting and adding some global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,17 +3,40 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   html, body, #root {
     min-height: 100%;
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
   }
   body {
     background: ${(props) => props.theme.colors.gray900};
     color: ${(props) => props.theme.colors.gray600};
     font-family: ${(props) => props.theme.fontFamily.sans};
-    padding: 1rem;
   }
 
   h1, h2, h3, h4, h5, h6 {
     font-family: ${(props) => props.theme.fontFamily.heading};
-    font-weight: bold;
     color: ${(props) => props.theme.colors.white};
+  }
+
+  ::-webkit-scrollbar {
+    width: 8px; /* Largura da barra de rolagem */
+  }
+
+  ::-webkit-scrollbar-track {
+      background: transparent; /* Cor de fundo da barra de rolagem */
+  }
+
+  ::-webkit-scrollbar-thumb {
+      background: ${({ theme }) => theme.colors.carbon}; /* Cor do "thumb" (alça) da barra de rolagem */
+      border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: ${({ theme }) => theme.colors.carbon}; /* Cor do "thumb" (alça) da barra de rolagem ao passar o mouse */
+  }
+
+  /* Cor da barra de rolagem ao ser arrastada */
+  ::-webkit-scrollbar-thumb:active {
+    background: ${({ theme }) => theme.colors.carbon};
   }
 `;
