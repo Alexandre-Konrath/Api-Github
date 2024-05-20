173db9d6fe418370fe4e3a18827946311421e9dd
Alexandre-Konrath <delimakonrath@gmail.com>
2024-05-20 14:18:05 -0300

    color configuration file for repository technologies

---
+++ src/services/config.js
@@ -0,0 +1,19 @@
+export const langColors = {
+  javascript: '#F7DF1E', // Amarelo para JavaScript
+  typescript: '#007ACC', // Azul para TypeScript
+  python: '#2ecc71', // Azul para Python
+  html: '#E34F26', // Laranja para HTML
+  css: '#7952B3', // Azul para CSS
+  bootstrap: '#7952B3', // Roxo para Bootstrap
+  sass: '#CC6699', // Rosa para Sass
+  php: '#777BB4', // Roxo para PHP
+  vue: '#4FC08D', // Verde para Vue.js
+  go: '#00ADD8', // Azul para Go
+  ruby: '#CC342D', // Vermelho para Ruby
+  c: '#A8B9CC', // Cinza para C
+  'c#': '#5C2D91', // Roxo escuro para C#
+  'c++': '#00599C', // Azul escuro para C++
+};
+
+
+// langColors['javascrypt'] = ira pegar a cor amarela do javascrypt
