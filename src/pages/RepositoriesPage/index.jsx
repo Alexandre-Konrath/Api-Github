f5d7f8580a04f3a6a82cd2f052567fcbcc7ef831
Alexandre-Konrath <delimakonrath@gmail.com>
2024-05-21 08:29:05 -0300

    small change on the main repositories page

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Profile from '../../components/Profile';
 import Filter from '../../components/Filter';
@@ -9,6 +9,7 @@ import { getLangsFrom } from '../../functions/getLangs';
 import { Container, Sidebar, Main } from './styles';
 
 const user = {
+
   login: "Alexandre-Konrath",
   name: "Alexandre de Lima Konrath",
   avatar_url: "https://avatars.githubusercontent.com/u/160286787?v=4",
@@ -21,36 +22,42 @@ const user = {
 
 const repositories = [
   {
+    id: '1',
     name: 'Repo 1',
     description: 'Descrição',
     html_url: 'https://youtube.com',
     language: 'JavaScript'
   },
   {
+    id: '2',
     name: 'Repo 2',
     description: 'Descrição',
     html_url: 'https://youtube.com',
     language: 'HTML'
   },
   {
+    id: '3',
     name: 'Repo 3',
     description: 'Descrição',
     html_url: 'https://youtube.com',
     language: 'CSS'
   },
   {
+    id: '4',
     name: 'Repo 4',
     description: 'Descrição',
     html_url: 'https://youtube.com',
     language: 'TypeScript'
   },
   {
+    id: '5',
     name: 'Repo 5',
     description: 'Descrição',
     html_url: 'https://youtube.com',
     language: null
   },
   {
+    id: '6',
     name: 'Repo 6',
     description: 'Descrição',
     html_url: 'https://youtube.com',
@@ -61,14 +68,21 @@ const repositories = [
 const languages = getLangsFrom(repositories)
 
 function RepositoriesPage() {
+  const [currentLanguage, setCurrentLanguage] = useState([])
+
+  // faz um filtro e recebe o language e seta ele
+  const onFilterClick = (language) => {
+    setCurrentLanguage(language)
+  }
+
   return (
     <Container>
       <Sidebar>
         <Profile user={user} />
-        <Filter languages={languages}/>
+        <Filter languages={languages} currentLanguage={currentLanguage} onClick={onFilterClick}/>
       </Sidebar>
       <Main>
-        <Repositories repositories={repositories} />
+        <Repositories repositories={repositories} currentLanguage={currentLanguage} onClick={onFilterClick}/>
       </Main>
     </Container>
   )
