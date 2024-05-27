ffdf5e235952e52bedb4c47a9ed4e78d3ee69ec3
Alexandre-Konrath <delimakonrath@gmail.com>
2024-05-27 08:36:56 -0300

    adicionando mais cores para o filtro de linguagens

--- src/services/config.js
+++ src/services/config.js
@@ -1,19 +1,156 @@
 export const langColors = {
+  'c#': '#178600', // Verde para C#
+  'c++': '#00599C', // Azul escuro para C++
+  'objective-c': '#438eff', // Azul para Objective-C
   javascript: '#F7DF1E', // Amarelo para JavaScript
-  typescript: '#007ACC', // Azul para TypeScript
-  python: '#2ecc71', // Azul para Python
+  python: '#2ecc71', // Verde para Python
   html: '#E34F26', // Laranja para HTML
-  css: '#7952B3', // Azul para CSS
+  css: '#563d7c', // Roxo para CSS
   bootstrap: '#7952B3', // Roxo para Bootstrap
   sass: '#CC6699', // Rosa para Sass
   php: '#777BB4', // Roxo para PHP
-  vue: '#4FC08D', // Verde para Vue.js
+  vue: '#42b883', // Verde para Vue.js
   go: '#00ADD8', // Azul para Go
-  ruby: '#CC342D', // Vermelho para Ruby
-  c: '#A8B9CC', // Cinza para C
-  'c#': '#5C2D91', // Roxo escuro para C#
-  'c++': '#00599C', // Azul escuro para C++
+  ruby: '#701516', // Vermelho para Ruby
+  c: '#555555', // Cinza para C
+  java: '#b07219', // Marrom para Java
+  swift: '#ffac45', // Laranja para Swift
+  kotlin: '#F18E33', // Laranja para Kotlin
+  rust: '#dea584', // Bege para Rust
+  scala: '#c22d40', // Vermelho para Scala
+  shell: '#89e051', // Verde claro para Shell
+  sql: '#e38c00', // Laranja para SQL
+  dart: '#00B4AB', // Verde água para Dart
+  r: '#198CE7', // Azul para R
+  perl: '#0298c3', // Azul para Perl
+  elixir: '#6e4a7e', // Roxo para Elixir
+  haskell: '#5e5086', // Roxo para Haskell
+  clojure: '#db5855', // Vermelho para Clojure
+  matlab: '#e16737', // Laranja para MATLAB
+  groovy: '#e69f56', // Laranja para Groovy
+  lua: '#000080', // Azul escuro para Lua
+  powershell: '#012456', // Azul escuro para PowerShell
+  julia: '#a270ba', // Roxo para Julia
+  coffeescript: '#244776', // Azul para CoffeeScript
+  typescript: '#007acc', // Azul para TypeScript
+  vbnet: '#945db7', // Roxo para VB.NET
+  crystal: '#000100', // Preto para Crystal
+  nim: '#37775b', // Verde para Nim
+  fortran: '#4d41b1', // Roxo para Fortran
+  fsharp: '#b845fc', // Roxo para F#
+  pascal: '#E3F171', // Amarelo para Pascal
+  assembly: '#6E4C13', // Marrom para Assembly
+  cobol: '#333399', // Azul escuro para COBOL
+  d: '#ba595e', // Vermelho para D
+  rpg: '#2BDE21', // Verde para RPG
+  ada: '#02f88c', // Verde para Ada
+  vbscript: '#15dcdc', // Verde água para VBScript
+  smalltalk: '#596706', // Verde musgo para Smalltalk
+  prolog: '#74283c', // Roxo para Prolog
+  ocaml: '#3be133', // Verde para OCaml
+  erlang: '#b83998', // Rosa para Erlang
+  lisp: '#3fb68b', // Verde para Lisp
+  purescript: '#1D222D', // Azul escuro para PureScript
+  vhdl: '#adb2cb', // Azul claro para VHDL
+  verilog: '#b2b7f8', // Azul claro para Verilog
+  abap: '#E8274B', // Vermelho para ABAP
+  actionscript: '#882B0F', // Marrom para ActionScript
+  apex: '#1797c0', // Azul para Apex
+  applescript: '#101F1F', // Preto para AppleScript
+  arc: '#aa2afe', // Roxo para Arc
+  aspectj: '#a957b0', // Roxo para AspectJ
+  awk: '#c30e9b', // Rosa para Awk
+  batchfile: '#C1F12E', // Amarelo para Batchfile
+  bro: '#ff7043', // Laranja para Bro
+  ceylon: '#dfa535', // Amarelo para Ceylon
+  chpl: '#8dc63f', // Verde para Chapel
+  cirru: '#ccccff', // Azul claro para Cirru
+  cmake: '#DA3434', // Vermelho para CMake
+  commonlisp: '#3fb68b', // Verde para Common Lisp
+  coq: '#d0b68c', // Bege para Coq
+  dhall: '#dfafff', // Rosa claro para Dhall
+  diff: '#88dddd', // Azul claro para Diff
+  dm: '#447265', // Verde escuro para DM
+  dockerfile: '#384d54', // Verde escuro para Dockerfile
+  dogescript: '#cca760', // Amarelo para Dogescript
+  dylan: '#6c616e', // Roxo escuro para Dylan
+  e: '#ccce35', // Amarelo para E
+  eex: '#6e4a7e', // Roxo para EEx
+  elm: '#60B5CC', // Azul claro para Elm
+  emacs: '#c065db', // Roxo para Emacs
+  ember: '#a01a24', // Vermelho para Ember
+  factor: '#636746', // Verde escuro para Factor
+  fancy: '#7b9db4', // Azul para Fancy
+  fennel: '#fff3d7', // Amarelo claro para Fennel
+  forth: '#341708', // Marrom escuro para Forth
+  frege: '#00cafe', // Azul claro para Frege
+  futhark: '#5f021f', // Marrom escuro para Futhark
+  gcode: '#D08CF2', // Roxo para G-code
+  gdb: '#a37102', // Marrom escuro para GDB
+  gdscript: '#355570', // Azul escuro para GDScript
+  gherkin: '#5B2063', // Roxo escuro para Gherkin
+  glyph: '#c1ac7f', // Bege para Glyph
+  gn: '#25be6a', // Verde para GN
+  golo: '#88562A', // Marrom para Golo
+  gosu: '#82937f', // Verde escuro para Gosu
+  grammars: '#ff0000', // Vermelho para Grammars
+  hack: '#878787', // Cinza para Hack
+  harbour: '#0e60e3', // Azul para Harbour
+  haxe: '#df7900', // Laranja para Haxe
+  hy: '#7790b2', // Azul claro para Hy
+  idris: '#b30000', // Vermelho para Idris
+  io: '#a9188d', // Roxo para Io
+  ioke: '#078193', // Verde escuro para Ioke
+  j: '#9EEDFF', // Azul claro para J
+  janet: '#88B8D6', // Azul claro para Janet
+  jq: '#c42559', // Vermelho para JQ
+  jsonnet: '#0064bd', // Azul escuro para Jsonnet
+  kusto: '#92f1a2', // Verde claro para Kusto
+  lasso: '#999999', // Cinza para Lasso
+  lex: '#DBCA00', // Amarelo para Lex
+  lolcode: '#cc9900', // Amarelo para LOLCODE
+  lookml: '#652B81', // Roxo escuro para LookML
+  m: '#0098d4', // Azul claro para M
+  makefile: '#427819', // Verde escuro para Makefile
+  markdown: '#083fa1', // Azul escuro para Markdown
+  marko: '#42bffb', // Azul claro para Marko
+  maxscript: '#00a6a6', // Verde água para MAXScript
+  solidity: '#3C3C3D', // Cinza escuro para Solidity
+  sparql: '#DDE072', // Amarelo claro para SPARQL
+  stata: '#1A5B96', // Azul escuro para Stata
+  stylus: '#ff6347', // Vermelho para Stylus
+  supercollider: '#46390b', // Marrom para SuperCollider
+  svelte: '#ff3e00', // Vermelho para Svelte
+  svg: '#ff9900', // Laranja para SVG
+  systemverilog: '#DAE1C2', // Verde claro para SystemVerilog
+  tcl: '#e4cc98', // Bege para Tcl
+  terra: '#00004c', // Azul escuro para Terra
+  tex: '#3D6117', // Verde para TeX
+  turing: '#45f715', // Verde claro para Turing
+  v: '#4f87c4', // Azul claro para V
+  vala: '#fbe5cd', // Bege claro para Vala
+  vba: '#867db1', // Roxo claro para VBA
+  velocity: '#507e35', // Verde escuro para Velocity
+  vim: '#199f4b', // Verde para Vim script
+  visualbasic: '#945db7', // Roxo para Visual Basic
+  wdl: '#42b983', // Verde para WDL
+  webassembly: '#654ff0', // Roxo para WebAssembly
+  wollok: '#a23738', // Vermelho para Wollok
+  x10: '#4B6BEF', // Azul para X10
+  xbase: '#403a40', // Roxo escuro para XBase
+  xml: '#0060ac', // Azul para XML
+  xojo: '#81bd41', // Verde claro para Xojo
+  xquery: '#5235A3', // Roxo escuro para XQuery
+  xslt: '#EB8CEB', // Rosa para XSLT
+  xtend: '#24255d', // Azul escuro para Xtend
+  zeek: '#ff7575', // Rosa claro para Zeek
+  zenscript: '#00BCD1', // Azul claro para ZenScript
+  zephir: '#118F9E', // Azul escuro para Zephir
+  zig: '#ec915c', // Laranja para Zig
+  zimpl: '#d67711', // Laranja escuro para Zimpl
 };
+// gerei tudo isso no chatGPT, se loco mt coisa.
 
 
-// langColors['javascrypt'] = ira pegar a cor amarela do javascrypt
+
+// por exemplo: langColors['javascrypt'] = ira pegar a cor amarela do javascrypt
